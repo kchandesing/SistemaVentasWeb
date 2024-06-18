1dc719451a7576c947440874aea516178d7f2a25
kchandesing <kevinchan851@gmail.com>
2024-06-18 14:29:09 -0600

    se actualizo el nombre del servlet Validar a ControladorValidar se reprogramo el codigo de acceso a la pagina principal cambiando el switch a ifs. se sigue implementado el desarrollo en el modulo empleado.

---
+++ src/java/Controlador/ControladorValidar.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import Modelo.EmpleadoDAO;
+import Modelo.Empleado;
+
+
+public class ControladorValidar extends HttpServlet {
+
+    EmpleadoDAO edao= new EmpleadoDAO();
+    Empleado em=new Empleado();
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+              
+  }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     String accion=request.getParameter("accion");
+    
+        if (accion.equalsIgnoreCase("ingresar")) {
+            String user=request.getParameter("txtuser");
+            String pass=request.getParameter("txtpass");
+            
+            em=edao.validar(user, pass);
+            
+            if (em.getUser() != null) {
+                request.setAttribute("usuario", em);
+                request.getRequestDispatcher("Controlador?menu=Principal").forward(request, response);
+            } else {
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+            }
+        } else {
+          request.getRequestDispatcher("index.jsp").forward(request, response);
+      }
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
